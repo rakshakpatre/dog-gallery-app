d93ba65690987424a9e92434021aed665b808652
rakshakpatre <rakshakpatre409@gmail.com>
2025-12-17 12:43:45 +0530

    Mobile UI Changes

--- frontend/dog-gallery-ui/src/services/api.js
+++ frontend/dog-gallery-ui/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-  // baseURL: "https://dog-gallery-app-8ry6.onrender.com",
+  // baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://dog-gallery-app-8ry6.onrender.com",
 });
 
 export default API;
